0ca586de11a3a5a18134f8453720da6b76075454
Dlgde <dlgdee@163.com>
2020-05-30 02:25:58 +0800

    add sync.map

---
+++ day04/syncMap/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
+
+var syncMap sync.Map
+var wg sync.WaitGroup
+
+func Addnumber(b int) {
+	for i := 0; i < b; i++ {
+		syncMap.Store(i, i)
+	}
+	wg.Done()
+}
+
+func main() {
+	m := 5
+	wg.Add(m)
+	var size int
+
+	for n := 0; n < m; n++ {
+		go Addnumber(n)
+	}
+
+	syncMap.Range(func(key, value interface{}) bool {
+		size++
+		return true
+	})
+
+	fmt.Println(strconv.Itoa(size))
+
+	v, ok := syncMap.Load(0)
+	if ok {
+		fmt.Println("key 0 has value", v, "")
+	}
+	// fmt.Println("there is wrong")
+
+	wg.Wait()
+
+}
